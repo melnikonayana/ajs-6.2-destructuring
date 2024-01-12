c9fcc07829b8e937c9f85aa9fec826b66fae90d4
Yana <bezotechestvo.yana@gmail.com>
2024-01-12 15:18:44 +0300

    initial commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,42 @@
+import getObjectFields from "../app";
+
+test('The function returns the extracted array of objects with four fields', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...'
+      }
+    ],
+  }
+
+  const special = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно'
+    }
+  ];
+
+expect(getObjectFields(character)).toEqual(special);
+})
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function getObjectFields(obj) {
+  return obj.special.map((item) => {
+    const {id, name, description = 'Описание недоступно', icon} = item;
+    return {id, name, description, icon};
+  })
+}
